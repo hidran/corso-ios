f0880958b3ea6ec687c3d9f29f63209ba878901b
Hidran Arias <hidran@gmail.com>
2019-08-20 02:26:48 +0200

    initial commit stackview login
    initial commit stackview login

---
+++ stackview-login/stackview-login/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  stackview-login
+//
+//  Created by Hidran Arias on 20/08/2019.
+//  Copyright © 2019 Hidran Arias. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
